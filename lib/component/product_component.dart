039b9516ce79213c566c31e57505bcf06c3ecaa1
luidimso <luidi_mso@outlook.com>
2021-01-14 12:44:35 -0300

    Style grid product component

--- lib/component/product_component.dart
+++ lib/component/product_component.dart
@@ -10,6 +10,45 @@ class ProductComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == "grid" ?
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 0.8,
+                child: Image.network(product.images[0],
+                  fit: BoxFit.cover
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(product.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Text("R\$ ${product.price.toStringAsFixed(2)}",
+                          style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+              )
+            ],
+          ) :
+          Row(),
+      ),
+    );
   }
 }
