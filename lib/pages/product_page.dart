46ad5b80c623ca11ded11f899d565c10f9542238
luidimso <luidi_mso@outlook.com>
2021-01-21 16:49:36 -0300

    Carousel of images

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_marketplace/interfaces/product_interface.dart';
 
@@ -17,6 +18,53 @@ class _ProductPageState extends State<ProductPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color mainColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((e) {
+                return NetworkImage(e);
+              }).toList(),
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotIncreasedColor: mainColor,
+              dotBgColor: Colors.transparent,
+              dotColor: mainColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(product.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                Text("R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: mainColor
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
