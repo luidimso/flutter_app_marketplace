e214426b889f6cb50aa5fac0d5e135deb5059f72
luidimso <luidi_mso@outlook.com>
2021-01-25 18:52:23 -0300

    Creating user on firebase

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,10 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class User extends Model {
   bool isLoading = false;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser firebaseUser;
+  Map<String, dynamic> user = Map();
 
-  void signup() {
+  void signup(Map<String, dynamic> user, String password, VoidCallback onSuccess, VoidCallback onFail) {
+    isLoading = true;
+    notifyListeners();
 
+    _auth.createUserWithEmailAndPassword(
+      email: user["email"],
+      password: password
+    ).then((value) async {
+      firebaseUser = value;
+
+      await _saveUser(user);
+
+      onSuccess();
+      isLoading = true;
+      notifyListeners();
+    }).catchError((error) {
+      onFail();
+      isLoading = true;
+      notifyListeners();
+    });
   }
 
   void signin() async {
@@ -17,5 +41,10 @@ class User extends Model {
 
   void recoverPassword() {
 
+  }
+
+  Future<Null> _saveUser(Map<String, dynamic> user) async {
+    this.user = user;
+    await Firestore.instance.collection("users").document(firebaseUser.uid).setData(user);
   }
 }
