03bd55bd5b54ef4adb53460641b9a7c2c3eb4763
luidimso <luidi_mso@outlook.com>
2021-02-10 12:22:37 -0300

    Places component

--- lib/component/place_component.dart
+++ lib/component/place_component.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PlaceComponent extends StatelessWidget {
 
@@ -9,6 +10,58 @@ class PlaceComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(
+            height: 100,
+            child: Image.network(snapshot.data["image"],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(snapshot.data["title"],
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17
+                  ),
+                ),
+                Text(snapshot.data["address"],
+                  textAlign: TextAlign.start
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  launch("geo:0,0?q=${snapshot.data["lat"]},${snapshot.data["log"]}");
+                },
+                child: Text("See on map"),
+                textColor: Colors.blue,
+                padding: EdgeInsets.zero,
+              ),
+              FlatButton(
+                onPressed: () {
+                  launch("tel:${snapshot.data["phone"]}");
+                },
+                child: Text("Call"),
+                textColor: Colors.blue,
+                padding: EdgeInsets.zero,
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
